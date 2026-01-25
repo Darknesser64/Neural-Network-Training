9fc541fcfc590929837b4ad18dda4e3e60f724ae
Darknesser64 <roland.danilovad@mail.ru>
2026-01-25 20:18:54 +0300

    UpgradePit.py

--- Pit.py
+++ Pit.py
@@ -1,3 +1,9 @@
 import torch
-print (torch.tensor([[2, 3], [4, 5]], dtype=torch.float32, requires_grad=True))
+tensor = torch.tensor([[[2, 3], [4, 5]], [[6, 7], [8, 9]]], dtype=torch.float32, requires_grad=True)
 
+# print (tensor.dtype)
+# print (tensor.ndim)
+# print (tensor [0, 0, 0])
+# print (type(tensor[0, 0, 0]))
+# print(tensor[0, 0, 0].item())
+print (type(tensor[0, 0, 0].item()))
